08e39c7e92e263c0539b1fda88c3d953f19891a0
Shar <81372629+shar27@users.noreply.github.com>
2024-11-17 13:14:28 +0100

    Update Contact.jsx

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -81,7 +81,7 @@ export default function Contact() {
                 <input type="hidden" name="current_url" />
 
                 <ReCAPTCHA
-                  sitekey="6LfV9nkqAAAAAKJA-KJdoNsvQDXNTbBDIxkDXKbp"
+                  sitekey="6LcCtoEqAAAAAD8zm7q5UcF3AoOaRUy9w_iBc2gP"
                   onChange={onReCAPTCHAChange}
                 />
               
